14ba05f027e891a98b2a3f34185ba9f4225aa3ee
manuel-spec <amaniase084@gmail.com>
2026-01-22 00:36:45 +0300

    feat: user-routes checkAuth

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  updateUserProfile,
+  getUserById,
+  getAllUsers,
+  deleteUser,
+  updateUserRole,
+  checkAuth,
+  updatePassword,
+} from "../controllers/userController.js";
+
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/check-auth", checkAuth); // Endpoint to check authentication status
+
+router.get("/", protect, admin, getAllUsers);
+router.get("/:id", protect, admin, getUserById);
+
+export default router;
