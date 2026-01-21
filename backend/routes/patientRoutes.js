a3ac6b8caf054222e7dc3d5dacf40c2e593eb995
manuel-spec <amaniase084@gmail.com>
2026-01-22 00:35:38 +0300

    feat: patient-routes-metrics

---
+++ backend/routes/patientRoutes.js
@@ -0,0 +1,18 @@
+// routes/patientRoutes.js
+import express from "express";
+import {
+  createPatient,
+  getPatientMetrics,
+  getAllPatients,
+} from "../controllers/patientController.js";
+import { uploadSingleScanImage } from "../middleware/upload.js";
+import { analyzeImage } from "../middleware/analyzeImage.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", protect, uploadSingleScanImage, analyzeImage, createPatient);
+router.get("/", protect, getAllPatients);
+router.get("/metrics", protect, getPatientMetrics);
+
+export default router;
