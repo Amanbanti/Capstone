e46b8f48422f23a60f330c0b1bf99f6545e13be6
manuel-spec <amaniase084@gmail.com>
2026-01-22 00:38:03 +0300

    feat: user-routes login-logout prefs

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -20,5 +20,8 @@ router.get("/check-auth", checkAuth); // Endpoint to check authentication status
 
 router.get("/", protect, admin, getAllUsers);
 router.get("/:id", protect, admin, getUserById);
+router.post("/", registerUser);
+router.post("/logout", logoutUser);
+router.post("/login", authUser);
 
 export default router;
